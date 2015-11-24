3525fdd2f6578d7c0c81848bbf73d92056ecd976
Matias Leidemer <matiasleidemer@gmail.com>
2015-11-24 20:56:28 -0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+ruby   '2.2.3'
+
+gem 'rake'
+gem 'lotus-router'
+gem 'lotus-controller'
+gem 'redis'
+gem 'puma'
+
+group :development, :test do
+  gem 'pry-byebug'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ spec/support/api.rb
@@ -0,0 +1,18 @@
+module ApiHelper
+  require 'rack/test'
+  include Rack::Test::Methods
+
+  private
+
+  def app
+    Bookshelf::V1::Application.new
+  end
+
+  def response
+    last_response
+  end
+end
+
+RSpec.configure do |config|
+  config.include ApiHelper, type: :api
+end
